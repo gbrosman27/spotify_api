0ff8a03e0c728a54bea0d92649491a980799b54a
Greg Brosman <greg.brosman@gmail.com>
2021-01-12 14:33:26 -0800

    Extracted playlist meta data. Created dataframe.

--- dataframe.py
+++ dataframe.py
@@ -1,7 +1,50 @@
-from spotify_data import results
+from spotify_data import *
+
 
 ids = []
 
+
+# Loop through the results from spotify_data.py. Append the 'track' and 'id' to the ids list.
 for item in results['tracks']['items']:
     track = item['track']['id']
     ids.append(track)
+
+
+# Song data to be displayed.
+song_meta = {'id':[], 'album':[], 'name':[], 'artist':[], 'explicit':[], 'popularity':[]}
+
+
+for song_id in ids:
+    meta = spot.track(song_id)
+
+    # Append song id to the id list in the song meta dict.
+    song_meta['id'].append(song_id)
+
+    # Album name
+    album = meta['album']['name']
+    song_meta['album'].append(album)
+
+    # Song name
+    song = meta['name']
+    song_meta['name'].append(song)
+
+    # Artist's name. List comprehension to grab artist name and join on comma/space.
+    s = ', '
+    artist = s.join([singer_name['name'] for singer_name in meta['artists']])
+    song_meta['artist'].append(artist)
+
+    # Explicit
+    explicit = meta['explicit']
+    song_meta['explicit'].append(explicit)
+
+    # Song popularity
+    popularity = meta['popularity']
+    song_meta['popularity'].append(popularity)
+
+
+# Create dataframe from the song metadata using pandas.
+song_meta_df = pd.DataFrame.from_dict(song_meta)
+
+print(song_meta_df)
+
+
--- spotify_data.py
+++ spotify_data.py
@@ -3,12 +3,17 @@ import pandas as pd
 import json
 from spotipy.oauth2 import SpotifyClientCredentials
 
+
+# IDs to access Spotify API
 client_id = "f607746d600d4ce9ac4089a44c832475"
 client_secret = "f94376c20039457e90d5d18b874d0955"
 
+
+# Pass the above IDs as arguments to spotipy to gain access.
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 spot = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-playlist_id = "spotify:playlist:37i9dQZF1EMdY30Sxip47Z"
 
+# ID of the playlist to be analyzed. Pass to spotipy.
+playlist_id = "spotify:playlist:37i9dQZF1EMdY30Sxip47Z"
 results = spot.playlist(playlist_id)
